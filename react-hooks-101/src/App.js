b54632d0fc45a93aea0e3719a24a51f3f36b676c
Kanon <mkc35r.0@gmail.com>
2024-04-04 15:29:58 +0900

    implement object state

--- react-hooks-101/src/App.js
+++ react-hooks-101/src/App.js
@@ -1,12 +1,8 @@
 import React, { useState } from 'react';
 
 const App = (props) => {
-  const [name, setName] = useState(props.name);
-  const [price, setPrice] = useState(props.price);
-  const reset = () => {
-    setPrice(props.price);
-    setName(props.name);
-  };
+  const [state, setState] = useState(props);
+  const { name, price } = state;
 
   return (
     <React.Fragment>
@@ -15,21 +11,30 @@ const App = (props) => {
       </p>
       <button
         onClick={() => {
-          setPrice(price + 1);
+          setState({ ...state, price: price + 1 });
         }}
       >
         +1
       </button>
       <button
         onClick={() => {
-          setPrice(price - 1);
+          setState({ ...state, price: price - 1 });
         }}
       >
         -1
       </button>
-      <button onClick={reset}>Reset</button>
+      <button
+        onClick={() => {
+          setState(props);
+        }}
+      >
+        Reset
+      </button>
 
-      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <input
+        value={name}
+        onChange={(e) => setState({ ...state, name: e.target.value })}
+      />
     </React.Fragment>
   );
 };
